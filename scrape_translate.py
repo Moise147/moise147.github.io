52c796c0663b4d1e82bca290b19d3a80e96961e9
Moise <fotografie.intuitiva@gmail.com>
2025-07-08 22:47:35 +0300

    Update scrape_translate.py

--- scrape_translate.py
+++ scrape_translate.py
@@ -3,14 +3,12 @@ from bs4 import BeautifulSoup
 from deep_translator import GoogleTranslator
 import json
 from datetime import datetime
-import re
 
 TELEGRAM_URLS = [
     "https://t.me/s/kpszsu?embed=1",
     "https://t.me/s/AMK_Mapping?embed=1"
     "https://t.me/s/southfronteng?embed=1"
 ]
-ACTIVATION_WORDS = ["У ніч", "Tu-22M3","Military Situation In Ukraine"]
 
 def scrape_messages(url):
     print(f"Accesăm URL-ul: {url}")
@@ -21,18 +19,10 @@ def scrape_messages(url):
     messages = []
     for div in soup.find_all("div", class_=lambda c: c and "tgme_widget_message_text" in c):
         text = div.get_text(separator="\n").strip()
-        if any(word.lower() in text.lower() for word in ACTIVATION_WORDS):
-            try:
-                translated_text = GoogleTranslator(source='auto', target='en').translate(text)
-                messages.append({
-                    "original": text,
-                    "translated": translated_text,
-                    "timestamp": datetime.utcnow().isoformat() + "Z"
-                })
-                print(f"Mesaj tradus: {translated_text}")
-            except Exception as e:
-                print(f"Eroare la traducere: {e}")
-                continue
+        messages.append({
+            "original": text,
+            "timestamp": datetime.utcnow().isoformat() + "Z"
+        })
     return messages
 
 def main():
@@ -40,11 +30,9 @@ def main():
     for url in TELEGRAM_URLS:
         all_messages.extend(scrape_messages(url))
 
-    # Adaugă data și ora rulării scriptului
-    script_run_time = datetime.utcnow().isoformat() + "Z"
     data = {
         "messages": all_messages,
-        "script_run_time": script_run_time
+        "script_run_time": datetime.utcnow().isoformat() + "Z"
     }
 
     with open("data.json", "w", encoding="utf-8") as f:
@@ -54,3 +42,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
